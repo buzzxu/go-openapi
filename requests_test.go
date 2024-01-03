ad83cca2b09e274c85f0c088110d0cfadac06fb6
buzzxu <xiang.leau@gmail.com>
2024-01-03 15:32:15 +0800

    init commit
    Signed-off-by: buzzxu <xiang.leau@gmail.com>

--- requests_test.go
+++ requests_test.go
@@ -8,9 +8,9 @@ func TestDoIt(t *testing.T) {
 	appkey := "xwjd"
 	secret := "XWJD010230441222212312313V"
 	result := make(map[string]interface{})
-	error := Request("https://api.mgr.xwjd.xingchenga.xyz/open/test", appkey, secret, "", &result, nil)
-	if error != nil {
-		t.Error(error)
+	err := Request("https://api.mgr.xwjd.xingchenga.xyz/open/test", appkey, secret, "", &result, nil)
+	if err != nil {
+		t.Error(err)
 	}
 	t.Log(result["data"])
 }
